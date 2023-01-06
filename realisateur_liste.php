0f31fac4f8b5ce3032d33fb5bf916d54196b65d4
Mohamad <mhdderi93@gmail.com>
2023-01-06 16:29:58 +0100

    créer la liste des realisateurs

--- realisateur_liste.php
+++ realisateur_liste.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="style.css">
 
 <?php
-
+// On se connecte à MySQL
 $db_real = new PDO ('mysql:host=localhost;dbname=cinema;charset=utf8', 'root', '');
 
+// On récupère les realisateur
 $requ_real = $db_real->prepare('SELECT p.nom_personne AS np, p.prenom_personne AS pp
 FROM personne p 
 INNER JOIN realisateur r ON r.id_personnage = p.id_personnage');
@@ -12,6 +13,8 @@ $requ_real->execute();
 
 $liste_real = $requ_real->fetchALL();
 ?>
+
+<!-- on crée une table pour afficher les information -->
 <table>
     <thead>
             <tr>
