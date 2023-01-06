75e342b584cf22ec9018cbe2949c50629c153cc0
Mohamad <mhdderi93@gmail.com>
2023-01-06 16:28:59 +0100

    créer la liste des genres des film

--- genres_liste.php
+++ genres_liste.php
@@ -1,15 +1,18 @@
 <link rel="stylesheet" href="style.css">
 
 <?php
-
+// On se connecte à MySQL
 $db_genre = new PDO ('mysql:host=localhost;dbname=cinema;charset=utf8', 'root', '');
 
+// On récupère les genres
 $requ_genre = $db_genre->prepare('SELECT libelle FROM genre ');
 
 $requ_genre->execute();
 
 $liste_genre = $requ_genre->fetchALL();
 ?>
+
+<!-- on crée une table pour afficher les information -->
 <table>
     <thead>
             <tr>
